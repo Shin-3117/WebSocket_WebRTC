47561a701e01b0d170bafacc060ea0ede5858da0
unknown <tlswnd3127@gmail.com>
2024-01-04 17:16:33 +0900

    first commit

---
+++ src/public/js/app.js
@@ -0,0 +1,28 @@
+//웹소켓
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form")
+
+socket.addEventListener("open", ()=>{
+  console.log("서버 연결 완료")
+})
+
+socket.addEventListener("message", (message)=>{
+  // console.log("msg:",message)
+  console.log("받은 메세지:",message.data)
+})
+
+socket.addEventListener("close", ()=>{
+  console.log("서버 연결 종료")
+})
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  console.log("보낸 메세지:",input.value)
+  socket.send(input.value)
+  input.value = ''
+}
+
+messageForm.addEventListener("submit", handleSubmit)
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname+"/public"))
+app.get("/", (req,res) => res.render("home"))
+app.get("/*", (req,res)=> res.redirect("/"))
+
+const handleListen = () => console.log("Listening on http://localhost:3000")
+// app.listen(3000, handleListen);
+
+//http서버에 웹소켓 서버 생성
+const server = http.createServer(app)
+const wss = new WebSocket.Server({server});
+
+const sockets = [];
+
+wss.on("connection", (socket)=>{
+  sockets.push(socket);
+  console.log('connect to Browser')
+  socket.on("close",()=>{
+    console.log('disconnect to Browser')
+  })
+  socket.on("message", (message)=>{
+    console.log(message.toString('utf-8'))
+    socket.send(message.toString('utf-8'));
+  });
+})
+
+
+server.listen(3000,handleListen);
